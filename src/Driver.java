aaa71630f040129057bab7c95cbe485d1b495c73
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2024-03-12 22:13:57 +0530

    finished

---
+++ src/Driver.java
@@ -0,0 +1,28 @@
+import  java.util.Scanner;
+public class Driver {
+    public static void main(String[]args){
+    System.out.println("enter the number of students:");
+    Scanner scan = new Scanner(System.in);
+    int numStudents = scan.nextInt();
+    String[] student = new String[numStudents];
+    int [] age = new int[numStudents];
+    Scanner scanName = new Scanner(System.in);
+    Scanner scanAge = new Scanner(System.in);
+
+    for (int i=0;i<numStudents; i++){
+        System.out.println("Enter name: ");
+        student[i] = scanName.nextLine();
+        System.out.println("Enter age: ");
+        age[i] = scanAge.nextInt();
+    }
+for (int i =0; i<numStudents; i++){
+    if (age[i] >=15){
+        System.out.println(student[i]+ "can receive the vaccination ");
+        System.out.println("The student is now "+age[i]);
+    }
+scan.close();
+    scanAge.close();
+    scanName.close();
+}
+    }
+}
