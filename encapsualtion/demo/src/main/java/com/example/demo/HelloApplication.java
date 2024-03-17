36776ea114dd80e19b35d220b6af02644740a420
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2024-03-17 10:50:47 +0530

    example

--- encapsualtion/demo/src/main/java/com/example/demo/HelloApplication.java
+++ encapsualtion/demo/src/main/java/com/example/demo/HelloApplication.java
@@ -7,17 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+public class HelloApplication {
+
 
     public static void main(String[] args) {
-        launch();
+        Student std = new Student();
+        std.setName("amani");
+       System.out.println("Name:"+std.getName());
     }
 }
---
+++ encapsualtion/demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Student {
+  private   String name;
+
+  public void setName(String studentName){
+      this.name = studentName;
+  }
+  public  String getName(){
+      return this.name;
+  }
+}
